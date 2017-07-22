315bbb95700ed9dea2ed229ead2f48adb2a3d9c1
julio vinachi <julio899@gmail.com>
2017-07-22 02:57:40 -0400

    correcciones y cambios en control

--- timer.js
+++ timer.js
@@ -1,7 +1,17 @@
+// define
+var CounterComponent = Vue.extend({
+	props:['tdia','hours','segunds','thours','tminutes','classMinutes','globalSegunds'],
+  	//template: '<span class="cb-time-area js-timer-color countdown-color-yellow"><div class="cb-time-text cb-hour inactive"><span class="js-cb-day">{{Math.trunc(globalSegunds/86400)}}</span><div class="cb-time-label">days</div></div><div class="time-separator invisible">:</div><div class="cb-time-text cb-hour inactive"><span class="js-cb-hour">{{thours}}</span><div class="cb-time-label">hrs</div></div>                        <div class="time-separator invisible">:</div>                        <div :class="classMinutes"><span class="js-cb-minute">{{tminutes}}</span><div class="cb-time-label">mins</div></div>   <div class="time-separator invisible">:</div>  <div class="cb-time-text cb-hour"><span class="js-cb-second">{{segunds}}</span><div class="cb-time-label">secs</div></div></span>',
+  	template:'<span>{{globalSegunds}} D:{{tdia}} h:{{thours}}</span>',
+})
+// register
+Vue.component('contador', CounterComponent)
+
 var contador = new Vue({
 	el:'#timer',
   data: {
     days:0,
+    tdia:0,
     hours:0,
     thours:0,
     minutes:0,
@@ -11,6 +21,7 @@ var contador = new Vue({
     tsegunds:0,
     interval:null,
     globalSegunds:null,
+    
   },
   created: function () {
   	this.getTime();
@@ -18,88 +29,155 @@ var contador = new Vue({
   mounted: function() {
   	var self = this;
         setInterval(function() {
-	           if(self.segunds>0)
-	           {
-	           		self.segunds--;
-	           }else{
-	           		if(self.globalSegunds>60){
-	           			self.segunds=60;	
-					    //self.minutes--;
-				        
-	           		}else{
-	           			self.segunds=self.globalSegunds;
-	           		}
-	           }
+        if(self.globalSegunds>0){
+        	self.globalSegunds--;
+        	if(Math.trunc(self.globalSegunds/86400)>0)
+        		self.tdia=Math.trunc(self.globalSegunds/86400);
+        	if(Math.trunc(self.globalSegunds/3600)>0)
+        		self.thours=Math.trunc(self.globalSegunds/3600);
+			
+			//Cantidad de Dias
+			//Math.trunc(self.globalSegunds/86400);        	
+        	//Cantidad de Horas self.thours=Math.trunc(self.globalSegunds/3600);
+        	//Cantidad de Horas self.tminutes=Math.trunc(Math.trunc(self.globalSegunds/60)/60);
+        	//Cantidad de Minutos 
+        	//Math.trunc(self.globalSegunds/60)
 
-           if(self.globalSegunds>0&&self.segunds>0){self.globalSegunds--;}
 
-	       //if(self.globalSegunds<60&&self.minutes>0){self.minutes--;}
-	       //if(self.globalSegunds<3600&&self.hours>0){self.hours--;}
-	       if(Math.trunc( self.globalSegunds/60 )< self.minutes && self.minutes>0){ self.minutes--; self.tminutes--; }
-	       if(Math.trunc( self.globalSegunds/3600 )< self.hours && self.hours>0)
-	       { 
-	       		self.hours--; 
-	       		if(self.days>0){self.thours=Math.trunc(self.hours/self.days); }else{self.thours=self.hours;}
-	       	}
-	       if(Math.trunc( self.globalSegunds/(3600*24) )< self.days && self.days>0){ self.days--; }
-/**
-        if(self.hours>0)
-        {
-        	self.tminutes=(self.hours*60);
-        }else{
-        	self.tminutes=self.minutes
-        }
-
-        //self.segunds++;
-        
-        if(self.minutes>0)
-        {
-        	self.tsegunds=(self.minutes*60);
-        }else{
-        	self.tsegunds=self.segunds; 
-        	if(self.tminutes<1)
-        	{
-	        	self.minutes='00';
-	        	self.classMinutes='cb-time-text cb-hour inactive';
+        	
+        	var porcentaje24h=100;	
+        	//var horas_seg=self.hours*3600;
+        	if(self.tdia>0){
+	        	porcentaje24h=self.getDecimal((self.globalSegunds/86400)/self.tdia);
+        		self.thours=Math.trunc(((porcentaje24h)*24)/100);
+        	}else{
+        		self.thours=Math.trunc(self.hours);
         	}
+        	// 100% -> 24h
+        	// 	99% -> ?
+		    var cantidadMinutes=0;
+        	if(self.thours>0)
+        		var cantidadMinutes=Math.trunc(Math.trunc(Math.trunc(self.globalSegunds/60)))-(self.thours*60);
+        	
+        	cantidadMinutes=Math.trunc(cantidadMinutes);
+        	
+        	//console.log('H:'+self.thours+' -m->'+(self.thours*60)+' *toS:'+(((self.globalSegunds/60)-(self.thours*60))/60)+' G:'+(self.globalSegunds/60))
+        	//console.log( (self.thours*60)-cantidadMinutes );
+        	//59min -> 100%
+			
+			//*********************************************
+			//MINUTOS
+			self.minutes=Math.trunc(self.globalSegunds/60);
+			//DIAS ***
+			self.days=Math.trunc(Math.trunc(Math.trunc(self.globalSegunds/60)/60)/24);
+			// /60 /24 dan los dias si quedan decimales son minutos restantes
+			// dias decimales ((self.minutes/60)/24)
+			//console.log(((self.minutes/60)/24)-self.days);
+			//24h -> 100%
+			//?   -> 0.99
+			//HORAS ***
+			self.thours=self.getDecimal(( (((self.minutes/60)/24)-self.days )*24)/100);
+			
 
-        }
+			//console.log( 't minutes '+self.minutes );
+			console.log( ((self.minutes/60)-60) );
+			//60m ->100%
+			//?		
+        	if( ((self.thours*60)-cantidadMinutes)>0 )
+				self.thours=Math.trunc(((self.globalSegunds/60)-(self.thours*60))/60);
 
 
-	        if(self.tsegunds<1){
-	           self.segunds=0;
-	        }else{
-	        	if(self.segunds==0)
-	        	{
-	           		if(self.minutes>0){self.minutes--;}
-	           		self.segunds=60;
-	        	}
-	           self.segunds--;
-	           self.tsegunds--;
-	        }
-*/
-        }, 1000);
+
+			
+			dec=((this.globalSegunds/60)-this.tminutes)*100;
+			this.segunds=Math.trunc( (dec*60)/100 );
+
+  				if(self.segunds>0)
+				{
+					self.segunds--;
+				}else{
+			           		if(self.globalSegunds>60){
+			           			self.segunds=59;						        
+			           		}else{
+			           			self.segunds=self.globalSegunds;
+			           		}
+			        }
+	
+        	}
+        	
+        	/*
+		self.tminutes=self.tminutes;
+        	self.dias=self.days;
+        	//Validacion de HORA
+        	if(self.thours > 0 && self.globalSegunds/(3600) < self.thours ){ self.thours--; self.hours--; }else{ self.thours=23; }
+  			          
+  			           if(self.segunds>0)
+			           {
+			           		self.segunds--;
+			           }else{
+			           		if(self.globalSegunds>60){
+			           			self.segunds=59;	
+							    //self.minutes--;
+							     
+							     // Minutes
+							     if(self.tminutes>0)
+							     {
+							     	self.tminutes--;	
+							     }else{
+							     	self.tminutes=59;
+							     }
+						        
+			           		}else{
+			           			self.segunds=self.globalSegunds;
+			           		}
+			           }
+
+		           if(self.globalSegunds>0&&self.segunds>0){self.globalSegunds--;}
+
+
+			       //if(self.globalSegunds<60&&self.minutes>0){self.minutes--;}
+			       //if(self.globalSegunds<3600&&self.hours>0){self.hours--;}
+			     
+
+			       if(Math.trunc( self.globalSegunds/(3600*24) )< self.days && self.days>0){ self.days--; }
+
+			       */
+        }, 100);
   },
   ready:function ready(){
   	
   },
   methods: {
+  getDecimal(numero){
+  	var a = Math.floor(numero * 100) / 100;
+  	 return (a.toFixed(2))*100;
+  	//return ((numero-Math.trunc(numero)).toFixed(2) )*100;
+  },
   getTime: function () {	
   	//buscar el total general de segundos
   	//con ese valor se desglosaran los demas valores
-  	this.globalSegunds=86401*1;
-  	this.minutes=Math.trunc(this.globalSegunds/60);
+  	this.globalSegunds=86400*3;//86405*2;
+  	if(Math.trunc(this.globalSegunds/60)>0)
+  		this.minutes=Math.trunc(this.globalSegunds/60);
+  	
   	this.hours=Math.trunc(this.minutes/60);
   	this.days=Math.trunc(this.hours/24);
   	this.tsegunds=this.globalSegunds;
 
-  	this.tminutes=Math.trunc(this.minutes/this.hours);
+  	//validacion de Minutos
+  	var decimalMinutos=(this.globalSegunds/this.minutes);
+	decimalMinutos=(decimalMinutos-(Math.trunc(decimalMinutos)))*10;
+	this.tminutes=Math.trunc(decimalMinutos);
+	// --- MINUTES --- //
+	console.log('minutes '+this.minutes)
+	console.log(this.globalSegunds/this.minutes)
+	// /60 /24 dan los dias si quedan decimales son minutos restantes 
 
 
-  	var decimalHora=(this.globalSegunds/this.days);
-  	decimalHora=((decimalHora/24)-3600)*10;
-  	this.thours=Math.trunc( (decimalHora*24)/100   );
-
+  	var decimalHora=(this.globalSegunds/3600);
+  	decimalHora=decimalHora/24;
+  	decimalHora=decimalHora-Math.trunc(decimalHora);
+  	this.thours=Math.trunc(decimalHora*10);
 
   	
   	if(this.globalSegunds>60&&this.minutes>0)
@@ -110,7 +188,7 @@ var contador = new Vue({
     }else{
     	this.segunds=this.globalSegunds;
     }
-  	console.log(this.globalSegunds);
-  }
+	console.log( this.thours );
+  },
  }
 })
